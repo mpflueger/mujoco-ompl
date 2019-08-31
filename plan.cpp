5919abf3262b2a84f75bd9d5cc5081df80a042fa
mpflueger <mpflueger@users.noreply.github.com>
2019-08-30 21:07:47 -0700

    read problem setup info from config

--- plan.cpp
+++ plan.cpp
@@ -9,6 +9,7 @@
 
 #include <ompl/control/SimpleSetup.h>
 #include <ompl/control/planners/sst/SST.h>
+#include "yaml-cpp/yaml.h"
 
 #include "mujoco_wrapper.h"
 
@@ -18,11 +19,38 @@ using namespace std;
 
 
 int main(int argc, char** argv) {
-    if (argc >= 1) {
+    string prob_config_filename = "";
+    if (argc >= 2) {
         string xml_filename = argv[1];
+        prob_config_filename = argv[2];
     } else {
-        cerr << "Please provide a MuJoCo config." << endl;
+        cerr << "Format: plan <MuJoCo XML config> <yaml problem spec> [time limit]"
+             << endl;
         return -1;
+    }
+
+    // Optional time limit
+    double timelimit = 1.0;
+    if (argc >= 3) {
+        stringstream ss;
+        ss << argv[3];
+        ss >> timelimit;
+    }
+
+    // Load yaml information
+    //   This should contain instructions on how to setup the planning problem
+    vector<double> start_vec;
+    vector<double> goal_vec;
+    if (prob_config_filename != "") {
+        YAML::Node node = YAML::LoadFile(prob_config_filename);
+
+        // Copy variables
+        if (node["start"]) {
+            start_vec = node["start"].as<vector<double> >();
+        }
+        if (node["goal"]) {
+            goal_vec = node["goal"].as<vector<double> >();
+        }
     }
 
     // Create MuJoCo Object
@@ -61,16 +89,19 @@ int main(int argc, char** argv) {
     oc::SimpleSetup ss(si);
     ss.setPlanner(sst_planner);
 
-    // TODO: Set start and goal states
-    /*******************
-    .
-    .
-    .
-    ****************/
+    // Set start and goal states
+    ob::ScopedState<> start_ss(ss.getStateSpace());
+    for(int i=0; i < start_vec.size(); i++) {
+        start_ss[i] = start_vec[i];
+    }
+    ob::ScopedState<> goal_ss(ss.getStateSpace());
+    for(int i=0; i < goal_vec.size(); i++) {
+        goal_ss[i] = goal_vec[i];
+    }
+    ss.setStartAndGoalStates(start_ss, goal_ss);
 
     // Call the planner
-    //ob::PlannerStatus solved = ss.solve(1.0);
-    bool solved = false;
+    ob::PlannerStatus solved = ss.solve(timelimit);
 
     if (solved) {
         cout << "Found Solution!" << endl;
